eb982b55186297e5a2fc0231529a26a738d1b082
Patryk Szymczyk <patszy97@interia.pl>
2019-05-22 12:15:42 +0200

    Object JS
    Added rectangle object to project.

--- script/animation2.js
+++ script/animation2.js
@@ -4,9 +4,11 @@ canvas.height=500;
 document.body.appendChild(canvas);
 var ctx = canvas.getContext('2d');
 
-var x = canvas.width/2;
-var y = canvas.height/2;
-var h = 40;
+var rectangle = {
+	x:canvas.width/2,
+	y:canvas.height/2,
+	h:Math.random()*20+30,
+}
 var fps = 20;
 
 var lastTime = 0;
@@ -20,10 +22,10 @@ function animationLoop(time){
 		lastTime  = time;
 		//ctx.clearRect(0, 0, canvas.width, canvas.height);
 		ctx.fillStyle = 'rgba(255, 255, 255, 0.2)';
-		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		ctx.fillRect(0, 0, rectangle.x, rectangle.y);
 		ctx.fillStyle = 'black';
-		x+=1;
-		y+=1;
-	 	ctx.fillRect(x-h/2, y-h/2, h, h);	
+		rectangle.x+=1;
+		rectangle.y+=1;
+	 	ctx.fillRect(rectangle.x-rectangle.h/2, rectangle.y-rectangle.h/2, rectangle.h, rectangle.h);	
 	 }
 }
