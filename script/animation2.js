451bfd8a8a396d0c4f3458464436542cf2c43b40
patszy <patszy97@interia.pl>
2019-05-23 11:21:59 +0200

    Fontain effect.
    Firework effect has been changed on better fountaing effect. Added gravity, deleting white objects, and object have blue colour.

--- script/animation2.js
+++ script/animation2.js
@@ -5,9 +5,11 @@ document.body.appendChild(canvas);
 var ctx = canvas.getContext('2d');
 
 var rectanglesArray = [];
+var visibleRectangles = [];
 
 var fps = 60;
 var lastTime = 0;
+var gravity = 1;
 animationLoop();
 
 function animationLoop(time){
@@ -17,32 +19,55 @@ function animationLoop(time){
 	if(time-lastTime >= 1000/fps){
 		lastTime  = time;
 
-		rectanglesArray.push({
-			x:canvas.width/2,
-			y:canvas.height/2,
-			h:rand(10, 100),
-			r:rand(0, 255),
-			g:rand(0, 255),
-			b:rand(0, 255),
-			speedx:rand(-10, 10),
-			speedy:rand(-10, 10),
-		})
-		while(rectanglesArray[rectanglesArray.length-1].speedx==0 && rectanglesArray[rectanglesArray.length-1].speedy==0){
-			rectanglesArray[rectanglesArray.length-1].speedx=rand(-10, 10);
-			rectanglesArray[rectanglesArray.length-1].speedy=rand(-10, 10);
-		}
-		//ctx.clearRect(0, 0, canvas.width, canvas.height); From earlier lesson.
 		ctx.fillStyle = 'rgba(255, 255, 255, 0.2)';
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+		visibleRectangles.length=0;
+
+		for(i=0; i<15; i++){
+			rectanglesArray.push({
+				x:canvas.width/2,
+				y:canvas.height/2,
+				h:rand(1, 10),
+				r:rand(0, 100),
+				g:rand(128, 255),
+				b:255,
+				speedx:rand(-10, 10),
+				speedy:rand(-10, 10),
+			})
+			while(rectanglesArray[rectanglesArray.length-1].speedx==0 && rectanglesArray[rectanglesArray.length-1].speedy==0){
+				rectanglesArray[rectanglesArray.length-1].speedx=rand(-10, 10);
+				rectanglesArray[rectanglesArray.length-1].speedy=rand(-10, 10);
+			}
+		}
+		//ctx.clearRect(0, 0, canvas.width, canvas.height); From earlier lesson.
+		
 		for(var i=0; i<rectanglesArray.length; i++){
 			var rectangle = rectanglesArray[i];
 			
 			ctx.fillStyle = 'rgb('+rectangle.r+','+rectangle.g+','+rectangle.b+')';
 			rectangle.x+=rectangle.speedx;
 			rectangle.y+=rectangle.speedy;
+			//Pseudo Gravity
+			rectangle.speedy+=gravity;
+
+			rectangle.r = Math.min(255, rectangle.r+5);
+			rectangle.g = Math.min(255, rectangle.g+5);
+			rectangle.b = Math.min(255, rectangle.b+5);
+
 		 	ctx.fillRect(rectangle.x-rectangle.h/2, rectangle.y-rectangle.h/2, rectangle.h, rectangle.h);
-		}		
+
+		 	if(rectangle.x+rectangle.h/2>0 && rectangle.x-rectangle.h/2<canvas.width &&
+		 	 	rectangle.y+rectangle.h/2>0 && rectangle.y-rectangle.h/2<canvas.height &&
+		 	 	(rectangle.r!=255 || rectangle.g!=255 || rectangle.b!=255)){
+		 		visibleRectangles.push(rectangle);
+		 	}
+		}
+
+		rectanglesArray = visibleRectangles.concat();
+		/*if(rectanglesArray.length>20){
+			rectanglesArray.shift();
+		}*/
 	}
 }
 
