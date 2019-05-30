8bd34dc251f67421aaa5a06dbe86f0e8cc7d4252
Patryk Szymczyk <patszy97@interia.pl>
2019-05-30 10:26:42 +0200

    Files preparation.

---
+++ script/script.js
@@ -0,0 +1,5 @@
+var canvas=document.createElement('canvas');
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+var ctx=canvas.getContext('2d');
+document.body.appendChild(canvas);
