54a651ac181d475ef298eb9c7d0ba22726f0b58f
PatSzy <patszy97@interia.pl>
2019-06-23 14:31:28 +0200

    Board

---
+++ script/Board.js
@@ -0,0 +1,38 @@
+Board.templates = [
+	[
+		'WWWWWWWWWWWWWWW',
+		'W             W',
+		'W X X X X X X W',
+		'W             W',
+		'W X X X X X X W',
+		'W             W',
+		'W X X X X X X W',
+		'W             W',
+		'W X X X X X X W',
+		'W             W',
+		'WWWWWWWWWWWWWWW',
+	],
+	[
+		'WWWWWWWWWWWWWWW',
+		'W             W',
+		'W X XXX XXX X W',
+		'W             W',
+		'W X X X X X X W',
+		'W X         X W',
+		'W X X X X X X W',
+		'W             W',
+		'W X XXX XXX X W',
+		'W             W',
+		'WWWWWWWWWWWWWWW',
+	]
+]
+
+Board.elemtnts = {
+	'floor':{sx:174, sy:16, type:'empty'},
+	'W':{sx:190, sy:16, type:'solid'},
+	'X':{sx:206, sy:16, type:'solid'},
+	'box':{sx:126, sy:16, type:'soft'},
+};
+function Board(){
+
+}
