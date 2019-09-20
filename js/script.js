ca588625dc4fc13dbc063f86a302eed4f6456797
patszy <patszy97@interia.pl>
2019-09-20 14:44:43 +0200

    Exercise 3 Random
    Write function which return random person from table: const students = ["John","Bill","Emma","Stella","Rob"]. This table should be an function argument.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,6 @@
-const person1 = {
-    name: "Jill",
-    age: 25,
-    hobby: "sport"
-};
-const person2 = {
-    name: null,
-    age: 25,
-    hobby: "sport"
-};
+const students = ["John","Bill","Emma","Stella","Rob"];
+window.onload = random(students);
 
-window.onload = sayHello(person1);
-
-function sayHello(object){
-    if(object.hasOwnProperty('name') && object.name!=null && object.name!=undefined) console.log("Hello: "+object.name);
-    else console.log("Hello: No Name.");
+function random(table){
+    console.log(table[Math.floor(Math.random()*table.length)]);
 }
