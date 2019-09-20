13fe3d1f6ab2bcd9630c4ef95b2bda3268a91585
patszy <patszy97@interia.pl>
2019-09-20 13:09:59 +0200

    Exercise 2 Objects
    Write the function sayHello which returns "Hello:, name" if object property: name exists. Example object: const person = {name:"Jill", age: 25, hobby: "sport"}.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-const numbers = [2, 5, 7, 10, 34, 16, 879, 1];
-function even(){
-    var table = [];
-    var n = 0;
-    for(i=0; i<numbers.length; i++){
-        if(numbers[i]%2==0){
-            table[n]=numbers[i];
-            n++;
-        }
-    }
-    console.log(table);
+const person1 = {
+    name: "Jill",
+    age: 25,
+    hobby: "sport"
+};
+const person2 = {
+    name: null,
+    age: 25,
+    hobby: "sport"
+};
+
+window.onload = sayHello(person1);
+
+function sayHello(object){
+    if(object.hasOwnProperty('name') && object.name!=null && object.name!=undefined) console.log("Hello: "+object.name);
+    else console.log("Hello: No Name.");
 }
